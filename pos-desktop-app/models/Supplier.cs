9cec02069e721a86b9b47566ae42b2a1b80e575e
Nova Devs <fogxcfg@gmail.com>
2024-07-24 13:46:08 +0530

    project structure improved

--- pos-desktop-app/models/Supplier.cs
+++ pos-desktop-app/models/Supplier.cs
@@ -6,7 +6,7 @@
         public string SupplierCode{ get; set; }
         public string SupplierName { get; set; }
         public string ContactPerson { get; set; }
-        public string Email{ get; set; }
+        public string Email { get; set; }
         public string Mobile1 { get; set; }
         public string Mobile2 { get; set; }
         public string Address { get; set; }
